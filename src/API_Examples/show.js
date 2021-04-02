5ad641b23f358891d758cf5561379cc1cf222ffa
Wassim Haddad <wass75@gmail.com>
2021-04-02 13:39:15 +0300

    insert commited

---
+++ src/API_Examples/show.js
@@ -0,0 +1,189 @@
+const data = {
+  backdrop_path: "/zXpSJLcczUt6EfbdULZanguzy87.jpg",
+  created_by: [
+    {
+      id: 52001,
+      credit_id: "5259743a760ee34661a5a053",
+      name: "Jon Bokenkamp",
+      gender: 2,
+      profile_path: null,
+    },
+  ],
+  episode_run_time: [43],
+  first_air_date: "2013-09-23",
+  genres: [
+    {
+      id: 18,
+      name: "Drama",
+    },
+    {
+      id: 80,
+      name: "Crime",
+    },
+    {
+      id: 9648,
+      name: "Mystery",
+    },
+  ],
+  homepage: "http://www.nbc.com/the-blacklist",
+  id: 46952,
+  in_production: true,
+  languages: ["en"],
+  last_air_date: "2021-03-26",
+  last_episode_to_air: {
+    air_date: "2021-03-26",
+    episode_number: 11,
+    id: 2754780,
+    name: "Captain Kidd",
+    overview:
+      'The task force attempts to stop an abduction by searching for a "treasure man" who uses geocaching to hide illicit goods and facilitate transactions between otherwise warring criminal factions.',
+    production_code: "811",
+    season_number: 8,
+    still_path: "/edLD4J8Fa0hQDo00JDnsAC61VeW.jpg",
+    vote_average: 0,
+    vote_count: 0,
+  },
+  name: "The Blacklist",
+  next_episode_to_air: {
+    air_date: "2021-04-02",
+    episode_number: 12,
+    id: 2798535,
+    name: "Rakitin",
+    overview:
+      "The task force's determination to identify a Russian asset in the U.S. government puts Cooper and Reddington in increasingly treacherous positions.",
+    production_code: "812",
+    season_number: 8,
+    still_path: "/55RvaL5tIclB2BIHPMRozHI6Qh.jpg",
+    vote_average: 0,
+    vote_count: 0,
+  },
+  networks: [
+    {
+      name: "NBC",
+      id: 6,
+      logo_path: "/o3OedEP0f9mfZr33jz2BfXOUK5.png",
+      origin_country: "US",
+    },
+  ],
+  number_of_episodes: 167,
+  number_of_seasons: 8,
+  origin_country: ["US"],
+  original_language: "en",
+  original_name: "The Blacklist",
+  overview:
+    'Raymond "Red" Reddington, one of the FBI\'s most wanted fugitives, surrenders in person at FBI Headquarters in Washington, D.C. He claims that he and the FBI have the same interests: bringing down dangerous criminals and terrorists. In the last two decades, he\'s made a list of criminals and terrorists that matter the most but the FBI cannot find because it does not know they exist. Reddington calls this "The Blacklist". Reddington will co-operate, but insists that he will speak only to Elizabeth Keen, a rookie FBI profiler.',
+  popularity: 180.047,
+  poster_path: "/htJzeRcYI2ewMm4PTrg98UMXShe.jpg",
+  production_companies: [
+    {
+      id: 11073,
+      logo_path: "/wHs44fktdoj6c378ZbSWfzKsM2Z.png",
+      name: "Sony Pictures Television",
+      origin_country: "US",
+    },
+  ],
+  production_countries: [],
+  seasons: [
+    {
+      air_date: "2013-09-10",
+      episode_count: 10,
+      id: 55083,
+      name: "Specials",
+      overview: "",
+      poster_path: "/5ZeJIlhrYO3zgW2JN62mi6mXkSM.jpg",
+      season_number: 0,
+    },
+    {
+      air_date: "2013-09-23",
+      episode_count: 22,
+      id: 55082,
+      name: "Season 1",
+      overview:
+        'For decades, ex-government agent Raymond "Red" Reddington has been one of the FBI’s Most Wanted fugitives. Brokering shadowy deals for criminals across the globe, Red was known by many as the "Concierge of Crime." Now, he’s mysteriously surrendered to the FBI with an explosive offer: he will help catch the world’s most elusive criminals, under the condition that he speaks only to Elizabeth "Liz" Keen, an FBI profiler fresh out of Quantico. For Liz, it’s going to be one hell of a first day on the job.',
+      poster_path: "/ccrCgW1ukxgwBue9ptkpCEjXE6q.jpg",
+      season_number: 1,
+    },
+    {
+      air_date: "2014-09-22",
+      episode_count: 22,
+      id: 61357,
+      name: "Season 2",
+      overview:
+        'For decades, ex-government agent Raymond "Red" Reddington has been one of the FBI\'s Most Wanted fugitives. He mysteriously surrendered to the FBI but now the FBI works for him as he identifies a "blacklist" of politicians, mobsters, spies and international terrorists. He will help catch them all... with the caveat that Elizabeth "Liz" Keen continues to work as his partner. Red will teach Liz to think like a criminal and "see the bigger picture"... whether she wants to or not.',
+      poster_path: "/b7o8AOpZPWJO8nP4SK8RRZ2d0B0.jpg",
+      season_number: 2,
+    },
+    {
+      air_date: "2015-10-01",
+      episode_count: 23,
+      id: 70935,
+      name: "Season 3",
+      overview:
+        "Now a fugitive on the run, Liz must figure out how to protect herself from the fallout of her actions in the explosive season two finale.",
+      poster_path: "/hWN5imHrpg8wjEBsa7v80sGsS8r.jpg",
+      season_number: 3,
+    },
+    {
+      air_date: "2016-09-22",
+      episode_count: 22,
+      id: 80082,
+      name: "Season 4",
+      overview:
+        "A mysterious man claiming to be Liz’s real father targets her, but first she must resolve the mystery of her lost childhood and reconcile her true identity with the elusive memories corrupted by Reddington. Without the truth, every day holds more danger for herself, her baby and her husband Tom. Meanwhile, the Task Force reels from Liz’s resurrection and friendships are fractured. Betrayed by those closest to him, Reddington’s specific moral code demands justice, all the while battling an army of new and unexpected blacklisters.",
+      poster_path: "/sDDajlDufTIoAoMW7WnHoZcQtSA.jpg",
+      season_number: 4,
+    },
+    {
+      air_date: "2017-09-27",
+      episode_count: 22,
+      id: 91328,
+      name: "Season 5",
+      overview:
+        "Feeling surprisingly unencumbered, Raymond Reddington is back, and in the process of rebuilding his criminal empire. His lust for life is ever-present as he lays the foundation for this new enterprise - one that he'll design with Elizabeth Keen by his side. Living with the reality that Red is her father, Liz finds herself torn between her role as an FBI agent and the temptation to act on her more criminal instincts. In a world where the search for Blacklisters has become a family trade, Red will undoubtedly reclaim his moniker as the “Concierge of Crime.”",
+      poster_path: "/6MupSEjQbWd5t37iHoYNGV1Rp2Y.jpg",
+      season_number: 5,
+    },
+    {
+      air_date: "2019-01-03",
+      episode_count: 22,
+      id: 112279,
+      name: "Season 6",
+      overview:
+        "Following the startling revelation that Raymond \"Red\" Reddington isn't who he says he is, Elizabeth Keen is torn between the relationship she's developed with the man assumed to be her father and her desire to get to the bottom of years of secrets and lies. Meanwhile, Red leads Liz and the FBI to some of the most strange and dangerous criminals yet, growing his empire and eliminating rivals in the process. All throughout, Liz and Red engage in an uneasy cat-and-mouse game in which lines will be crossed and the truth will be revealed.",
+      poster_path: "/f1R6R8AVS8EwaOUltlVxNjASZAs.jpg",
+      season_number: 6,
+    },
+    {
+      air_date: "2019-10-04",
+      episode_count: 19,
+      id: 132066,
+      name: "Season 7",
+      overview:
+        'After being abducted by Katarina Rostova, Raymond "Red" Reddington finds himself alone in hostile territory, unsure of who, if anyone, he can trust. Surrounded by old enemies and new allies, Red must stay one step ahead of the Blacklist\'s most dangerous criminal, who will stop at nothing to unearth the very truth Red wants no one to know about. To find it, Katarina will insinuate herself into the life of Elizabeth Keen, who has finally reunited with her daughter Agnes. Katarina’s presence will bring danger to Liz’s doorstep and forever alter her relationship with Red.',
+      poster_path: "/e72ZZsSeKADafuy2aPWCUp15GgE.jpg",
+      season_number: 7,
+    },
+    {
+      air_date: "2020-11-13",
+      episode_count: 15,
+      id: 165869,
+      name: "Season 8",
+      overview:
+        "With his back against the wall, Raymond Reddington faces his most formidable enemy yet: Elizabeth Keen. Aligned with her mother, infamous Russian spy Katarina Rostova, Liz must decide how far she is willing to go to find out why Reddington has entered her life and what his endgame really is. The fallout between Reddington and Keen will have devastating consequences for all that lie in their wake, including the Task Force they helped to create.",
+      poster_path: "/htJzeRcYI2ewMm4PTrg98UMXShe.jpg",
+      season_number: 8,
+    },
+  ],
+  spoken_languages: [
+    {
+      english_name: "English",
+      iso_639_1: "en",
+      name: "English",
+    },
+  ],
+  status: "Returning Series",
+  tagline: "Family. Business.",
+  type: "Scripted",
+  vote_average: 7.4,
+  vote_count: 1912,
+};
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -42,6 +42,30 @@ const Search = () => {
                   src={posterPath + result.poster_path}
                   alt="poster"
                 />
+                <div>
+                  <div className="search-result-info">
+                    <div className="result-overview">
+                      <span>Overview: </span>
+                      {result.overview}
+                    </div>
+                    <div className="result-first-aired">
+                      <span>First aired: </span>
+                      {result.first_air_date}
+                    </div>
+                    <div className="result-average-vote">
+                      <span>Average vote: </span>
+                      {result.vote_average}
+                    </div>
+                  </div>
+
+                  <div className="show-result-buttons">
+                    <button className="add-to-watchlist">
+                      Add to Watch List
+                    </button>
+                    <button className="watch-trailer">Watch Trailer</button>
+                    <button className="simillar-shows">Show Similar</button>
+                  </div>
+                </div>
               </div>
             ))
           : null}
